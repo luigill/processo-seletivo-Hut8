4315cdb653af0e666a81a842b6f1455bbf61b9ea
Lui Gill Aquini <luigill@pop-os.localdomain>
2022-08-02 16:48:34 -0300

    Primeiras aulas.

---
+++ semana2/aulas/strings.js
@@ -0,0 +1,12 @@
+let value = 'Lui Gill';
+
+console.log(value[0]);
+console.log(value[1]);
+
+for (let i = 0; i < value.length; i++) {
+  console.log(value[i]);
+}
+
+value[0] = 'M';
+console.log(value);
+console.log(value[0]);
---
+++ semana2/aulas/tipo-e-cast.js
@@ -0,0 +1,10 @@
+//Tipo da Variável e Cast
+let value = '10';
+let valueTwo = '10';
+console.log(typeof value);
+//Cast para tipo number
+value = Number(value);
+console.log(typeof value);
+
+console.log(value + valueTwo);
+console.log(Number(value) + Number(valueTwo));
