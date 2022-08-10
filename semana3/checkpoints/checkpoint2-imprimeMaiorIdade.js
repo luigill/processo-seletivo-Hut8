69f472393233be9c0e84e72558844490067bc8c3
Lui Gill Aquini <luigill@pop-os.localdomain>
2022-08-10 12:41:04 -0300

    Adicionando descontrução no checkpoint

--- semana3/checkpoints/checkpoint2-imprimeMaiorIdade.js
+++ semana3/checkpoints/checkpoint2-imprimeMaiorIdade.js
@@ -3,9 +3,14 @@ let user = {
   idade: 28
 };
 
+//exemplo usando destructuring
+//const { idade } = user;
+
 const maiorIdade = object =>
+  //idade < 18
   object.idade < 18
     ? console.log('Menor de idade...')
     : console.log('É maior de idade!');
 
 maiorIdade(user);
+//maiorIdade(idade);
